df17ac5fa573615c7fb9387bf112adb2207262f8
Justin Bonnici <justin-edward.bonnici@gov.mt>
2023-01-26 13:36:38 +0100

    Initial commit

---
+++ white-noise-player-app/src/main/java/com/whitenoiseplayer/app/WhiteNoisePlayerAppApplication.java
@@ -0,0 +1,13 @@
+package com.whitenoiseplayer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteNoisePlayerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteNoisePlayerAppApplication.class, args);
+	}
+
+}
---
+++ white-noise-player-app/src/test/java/com/whitenoiseplayer/app/WhiteNoisePlayerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitenoiseplayer.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteNoisePlayerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
